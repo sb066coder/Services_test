9c8bf8d772d2b1a7d952922e5c4d710bf9c44d3e
sergey <sergey066@yandex.ru>
2023-04-25 15:24:05 +0300

    Notification added

--- app/src/main/java/ru/sb066coder/servicestest/MainActivity.kt
+++ app/src/main/java/ru/sb066coder/servicestest/MainActivity.kt
@@ -1,5 +1,8 @@
 package ru.sb066coder.servicestest
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.sb066coder.servicestest.databinding.ActivityMainBinding
@@ -13,7 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.btnSimpleService.setOnClickListener {
-            startService(MyService.newIntent(this))
+            startService(MyService.newIntent(this, 25))
         }
+        binding.btnForegroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val notificationChannel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(notificationChannel)
+        val notification = Notification.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+        notificationManager.notify(1, notification)
+    }
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
