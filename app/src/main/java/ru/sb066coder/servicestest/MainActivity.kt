d2e5b278242629bf559e5024ddd2d01b314023c3
sergey <sergey066@yandex.ru>
2023-04-25 13:48:52 +0300

    Simple service added

--- app/src/main/java/ru/sb066coder/servicestest/MainActivity.kt
+++ app/src/main/java/ru/sb066coder/servicestest/MainActivity.kt
@@ -12,5 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.btnSimpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
     }
 }
