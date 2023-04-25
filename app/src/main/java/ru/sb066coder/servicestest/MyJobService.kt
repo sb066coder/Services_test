660771790d39246c9e99456df269b92f5ce41d21
sergey <sergey066@yandex.ru>
2023-04-25 17:53:46 +0300

    Job service added

---
+++ app/src/main/java/ru/sb066coder/servicestest/MyJobService.kt
@@ -0,0 +1,43 @@
+package ru.sb066coder.servicestest
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyJobService : JobService() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        log("onStartJob")
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer: $i")
+            }
+            jobFinished(params, true)
+        }
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+        log("onDestroy")
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyJobService: $message")
+    }
+}
